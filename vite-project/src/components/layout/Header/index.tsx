5672b2253910ea1e19d7fc776592af7fee4a1edd
Rikiub <rikiub.dev@gmail.com>
2025-11-12 16:15:07 -0400

    chore(Header): Use react-router <Link> instead of html <a>

--- vite-project/src/components/layout/Header/index.tsx
+++ vite-project/src/components/layout/Header/index.tsx
@@ -1,40 +1,36 @@
 import { Code } from "lucide-react";
+import { Link } from "react-router";
 import styles from "./styles.module.css";
 
 export function Header() {
 	return (
 		<header className={styles.root}>
-			<Logo />
-			<Nav />
-			<Action />
+			<div className={styles.logo}>
+				<Code size={30} />
+				<p>DevJobs</p>
+			</div>
+
+			<nav className={styles.nav}>
+				<Link to={{ pathname: "/" }} viewTransition>
+					Inicio
+				</Link>
+
+				<Link to={{ pathname: "/empleos" }} viewTransition>
+					Empleos
+				</Link>
+
+				<Link to={{ pathname: "/empresas" }} viewTransition>
+					Empresas
+				</Link>
+
+				<Link to={{ pathname: "/salarios" }} viewTransition>
+					Salarios
+				</Link>
+			</nav>
+
+			<div className={styles.action}>
+				<a href="#top">Publicar empleo</a>
+			</div>
 		</header>
-	);
-}
-
-function Logo() {
-	return (
-		<div className={styles.logo}>
-			<Code size={30} />
-			<p>DevJobs</p>
-		</div>
-	);
-}
-
-function Nav() {
-	return (
-		<nav className={styles.nav}>
-			<a href="/">Inicio</a>
-			<a href="/empleos">Empleos</a>
-			<a href="#top">Empresas</a>
-			<a href="#top">Salarios</a>
-		</nav>
-	);
-}
-
-function Action() {
-	return (
-		<div className={styles.action}>
-			<a href="#top">Publicar empleo</a>
-		</div>
 	);
 }
