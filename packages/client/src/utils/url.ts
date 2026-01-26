2adff49538df71658cdd301a07cc8d284449c18e
Rikiub <rikiub.dev@gmail.com>
2026-01-26 12:05:37 -0400

    chore(client): Remove utils folder

--- packages/client/src/utils/url.ts
+++
@@ -1,9 +0,0 @@
-export function serializeObject(
-	// biome-ignore lint/suspicious/noExplicitAny: <helper>
-	obj: Record<string, any>,
-): Record<string, string> {
-	const result = Object.fromEntries(
-		Object.entries(obj).map(([key, value]) => [key, String(value)]),
-	);
-	return result;
-}
